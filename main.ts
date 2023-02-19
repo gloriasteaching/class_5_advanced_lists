8596530da9f2f83674521a8ad7c928a00bd3b708
gloriasteaching <47659579+gloriasteaching@users.noreply.github.com>
2023-02-19 13:05:56 +0100

    Find index

--- main.ts
+++ main.ts
@@ -9,3 +9,5 @@ let text_list = ["amongus", "is", "cool"]
 let empty_list: number[] = []
 game.splash(text_list.removeAt(0))
 info.setScore(text_list.length)
+game.splash(text_list)
+info.setScore(text_list.indexOf("cool"))
